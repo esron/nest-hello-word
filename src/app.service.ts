e43704575a0e6663730ce35703aac477b01419a9
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-18 20:48:09 -0300

    return a random word

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private words = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune',
+    // Pluto is not a planet
+  ];
+
   getHello(): string {
-    return 'Hello World!';
+    const index = Math.floor(Math.random() * 8);
+
+    return this.words[index];
   }
 }
